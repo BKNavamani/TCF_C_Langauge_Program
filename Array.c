8d888b1392c98b0a5f5c9755b63a515460b22d8d
Navamani <navamanib01@gmail.com>
2025-08-07 14:12:55 +0530

    Array.c

--- Array.c
+++ Array.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array elements are: ");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+        sum += arr[i];
+    }
+    printf("\n");
+
+    printf("Sum of array elements: %d\n", sum);
+
+    return 0;
+}
 
