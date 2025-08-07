4cb657a9a9f7a3519aaa3e9efaa70b8a562bc7b7
Navamani <navamanib01@gmail.com>
2025-08-07 14:14:29 +0530

    Pointers.c

--- Pointers.c
+++ Pointers.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+
+void displayArray(int *arr, int n);
+int sumArray(int *arr, int n);
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Array elements are: ");
+    displayArray(arr, n);
+
+    int sum = sumArray(arr, n);
+    printf("Sum of array elements: %d\n", sum);
+
+    return 0;
+}
+
+void displayArray(int *arr, int n) {
+    for(int i = 0; i < n; i++) {
+        printf("%d ", *(arr + i));
+    }
+    printf("\n");
+}
+
+int sumArray(int *arr, int n) {
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        sum += *(arr + i);
+    }
+    return sum;
+}
 
